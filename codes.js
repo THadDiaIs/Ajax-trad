c7faa2ff9d32e57667699175d9fbfee0bd7e501e
AdolfHd <sdia7adia@gmail.com>
2023-11-26 14:06:58 +0500

    improved iu and some menor fixes

--- codes.js
+++ codes.js
@@ -12,12 +12,12 @@ function work() {
   //   });
   // });
   $('#word').keypress((event)=>{
-    $('.results').html('<div class="line"><span class="frst">something-here</span><span class="scond">some-thingheretoo</span></div>');
+    $('.results').html('<div class="line"><span class="frst">something-here</span><span class="scond">something does not work</span></div>');
 
   });
   $('#btn').click((event)=>{
     event.preventDefault();
-    $('.results').html('<div class="line"><span class="frst">something-here</span><span class="scond">some-thingheretoo</span></div>');
+    $('.results').html('<div class="line"><span class="frst">something-here</span><span class="scond">Cannot retrive data.</span></div>');
 
   });
 
